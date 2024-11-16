72f8710e7a1cd9131c12f4cdd3b4d0b8d8691b5f
Jerry <you@example.com>
2024-11-16 10:57:47 +0100

    done assignments 6-10

--- js/script.js
+++ js/script.js
@@ -12,13 +12,37 @@ headerNav.children[2].innerHTML = "Mail Us";
 // 4.
 const art2 = document.querySelector(".art-2");
 art2.querySelector("p").innerHTML =
-  "A vibrant fire-red hoodie made from soft, breathable cotton blend."
-  + " It features a cozy adjustable drawstring hood and hem for a snug fit.";
+  "A vibrant fire-red hoodie made from soft, breathable cotton blend." +
+  " It features a cozy adjustable drawstring hood and hem for a snug fit.";
 
 // 5.
 const art1Button = art1.querySelector("button");
 art1Button.innerHTML = "add to cart";
 art1Button.style.backgroundColor = "red";
 
-//6. 
+// 6.
+art1.querySelector("figure").style.backgroundColor = "red";
 
+// 7.
+const footer = document.querySelector("footer section");
+footer.children[1].querySelector("p").innerHTML =
+  "Sinus skateboards<br>Malmöleden 22 <br>320 20, Malmö";
+
+// 8. 
+const allPTags = document.querySelectorAll("body p");
+allPTags.forEach(paragraph => {
+    paragraph.style.color = "blue";
+});
+
+// 9.
+const allButtons = document.querySelectorAll("button");
+allButtons.forEach(button => {
+    button.innerHTML = "add to cart"
+});
+
+// 10. 
+const menuHome = footer.querySelector("article a");
+menuHome.classList.add("active"); 
+console.log(menuHome);
+
+// 11. 
